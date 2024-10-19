6aa10b85d7886339440a53d63eefd0247246d948
qiuqiu <2371824615@qq.com>
2024-10-19 15:15:47 +0800

    login

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,12 @@
 <script setup>
-import { useUserStore } from './stores'
-const userStore = useUserStore()
+
+
 </script>
 
 <template>
-  <div>
-    APP
-    <hr>
-    <router-view></router-view>
-    <hr>
-    <hr>
-    <hr>
-  </div>
 
-  <el-button @click="$router.push('/home')">首页</el-button>
-  <div>
-    <p> {{ userStore.token }}</p>
-    <el-button @click="userStore.setToken('asdas')" type="success">token</el-button>
-    <el-button @click="userStore.removeToken()" type="info">remove</el-button>
-  </div>
+  <router-view></router-view>
+
 
 </template>
 
