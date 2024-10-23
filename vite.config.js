75967886ebe7a743befbb380da785be0c3215d43
qiuqiu <2371824615@qq.com>
2024-10-23 22:42:45 +0800

    更改build路径

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 // https://vite.dev/config/
 export default defineConfig({
+  base: './',  // 添加这一行，设置基础路径为相对路径
   plugins: [
     vue(),
     AutoImport({
